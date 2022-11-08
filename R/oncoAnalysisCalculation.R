7995822696c8b6bdf80ea8ed3fa00ad310e0371c
Xu Xinyi <tiffanyandjojo.xu@mail.utoronto.ca>
2022-11-07 20:42:13 -0500

    Modified DESCRIPTION
    Added the DESCRIPTION contents

--- R/oncoAnalysisCalculation.R
+++
@@ -1,7 +0,0 @@
-# Install Required packages
-install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
-library(devtools)
-library(roxygen2)
-library(testthat)
-library(knitr)
-devtools::has_devel() # Check the system status
