a16164c2d8188acd13b6afd5a21909be16e40855
Xu Xinyi <tiffanyandjojo.xu@mail.utoronto.ca>
2022-11-07 22:04:35 -0500

    Added the correct NAMESPACE

--- R/oncoAnalysisCalculation.R
+++ R/oncoAnalysisCalculation.R
@@ -1,7 +1,7 @@
 # Install and Load the required packages
-install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
+# install.packages(c("devtools", "roxygen2", "testthat", "knitr"))
 library(devtools)
 library(roxygen2)
 library(testthat)
 library(knitr)
-devtools::has_devel() # Check if successfully installed
+# devtools::has_devel() # Check if successfully installed
