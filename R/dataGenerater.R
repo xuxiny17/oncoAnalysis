b75023c4e3267b9d400150f41f4ccde42f890daf
Xu Xinyi <tiffanyandjojo.xu@mail.utoronto.ca>
2022-11-07 23:18:00 -0500

    Added the empty function files

---
+++ R/dataGenerater.R
@@ -0,0 +1,14 @@
+#' Generate a mutated FASTA File and List Variable based on Healthy Version
+#'
+#'
+#'
+#'
+#'
+#'
+#' @export
+#'
+dataGenerater <- function(data, nummut) {
+  return()
+}
+
+#' [END]
