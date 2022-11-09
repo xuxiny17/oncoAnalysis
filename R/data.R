cea1096a1ee7070295ac4ebbac834ce4bc2057ca
Xu Xinyi <tiffanyandjojo.xu@mail.utoronto.ca>
2022-11-09 00:11:15 -0500

    Added the description of Data set

---
+++ R/data.R
@@ -0,0 +1,38 @@
+#' Data set containing Sample DNA Sequences (Healthy)
+#'
+#' A Healthy Sample DNA Sequences(P53)
+#'
+#' @source National Library of Medicine.
+#'
+#' @format A vector containing strings, each string contains one base
+#' \describe{
+#'  \item{Base.1}{A DNA base}
+#'  \item{Base.2}{A DNA base}
+#'  \item{Base.N}{A DNA base}
+#' }
+#' @examples
+#' \dontrun{
+#'  sampleseq
+#' }
+"sampleseq"
+
+#' Data set containing Sample DNA Sequences (Mutated)
+#'
+#' A Mutated Sample DNA Sequences(P53)
+#'
+#' @source National Library of Medicine, and self generated.
+#'
+#' @format A vector containing strings, each string contains one base
+#' \describe{
+#'  \item{Base.1}{A DNA base}
+#'  \item{Base.2}{A DNA base}
+#'  \item{Base.N}{A DNA base}
+#' }
+#' @examples
+#' \dontrun{
+#'  samplemutseq
+#' }
+"samplemutseq"
+
+# [END]
+
